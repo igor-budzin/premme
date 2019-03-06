7d4bb7e8cfe8f5b14ad495a6c5d0f5e25d809fcc
Ігор <igor.budzin95@gmail.com>
2019-03-06 16:40:20 +0200

    tabs

--- gulpfile.js
+++ gulpfile.js
@@ -1,68 +1,68 @@
 var gulp = require("gulp"),
-	sass = require("gulp-sass"),
-	postcss = require("gulp-postcss"),
-	autoprefixer = require("autoprefixer"),
-	cssnano = require("cssnano"),
-	sourcemaps = require("gulp-sourcemaps"),
-	imagemin = require('gulp-imagemin'),
-	cache = require('gulp-cache'),
-	browserSync = require("browser-sync").create();
+  sass = require("gulp-sass"),
+  postcss = require("gulp-postcss"),
+  autoprefixer = require("autoprefixer"),
+  cssnano = require("cssnano"),
+  sourcemaps = require("gulp-sourcemaps"),
+  imagemin = require('gulp-imagemin'),
+  cache = require('gulp-cache'),
+  browserSync = require("browser-sync").create();
 
 var paths = {
-	styles: {
-		src: "src/scss/*.scss",
-		dest: "dist/css"
-	},
-	images: {
-		src: 'src/images/*.+(png|jpg|gif|svg)',
-		dest: 'dist/images'
-	},
-	html: {
-		src: 'src/*.html',
-		dest: 'dist'
-	}
+  styles: {
+    src: "src/scss/*.scss",
+    dest: "dist/css"
+  },
+  images: {
+    src: 'src/images/*.+(png|jpg|gif|svg)',
+    dest: 'dist/images'
+  },
+  html: {
+    src: 'src/*.html',
+    dest: 'dist'
+  }
 };
 
 function style() {
-	return gulp
-		.src(paths.styles.src)
-		.pipe(sourcemaps.init())
-		.pipe(sass())
-		.on("error", sass.logError)
-		.pipe(postcss([autoprefixer(), cssnano()]))
-		.pipe(sourcemaps.write())
-		.pipe(gulp.dest(paths.styles.dest))
-		.pipe(browserSync.stream());
+  return gulp
+    .src(paths.styles.src)
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .on("error", sass.logError)
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(paths.styles.dest))
+    .pipe(browserSync.stream());
 }
 
 function image() {
-	return gulp
-		.src(paths.images.src)
-		.pipe(gulp.dest(paths.images.dest))
-		.pipe(browserSync.stream());
+  return gulp
+    .src(paths.images.src)
+    .pipe(gulp.dest(paths.images.dest))
+    .pipe(browserSync.stream());
 }
 
 function reload(done) {
-	browserSync.reload();
-	done();
+  browserSync.reload();
+  done();
 }
 
 function html() {
-	return gulp
-		.src(paths.html.src)
-		.pipe(gulp.dest(paths.html.dest))
-		.pipe(browserSync.stream());
+  return gulp
+    .src(paths.html.src)
+    .pipe(gulp.dest(paths.html.dest))
+    .pipe(browserSync.stream());
 }
 
 function watch() {
-	browserSync.init({
-		server: {
-			baseDir: "./dist"
-		}
-	});
-	gulp.watch(paths.styles.src, style);
-	gulp.watch(paths.html.src, html);
-	gulp.watch(paths.images.src, image);
+  browserSync.init({
+    server: {
+      baseDir: "./dist"
+    }
+  });
+  gulp.watch(paths.styles.src, style);
+  gulp.watch(paths.html.src, html);
+  gulp.watch(paths.images.src, image);
 }
 
 exports.watch = watch
